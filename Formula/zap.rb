6c8ead41ac34b49e1768b52c832cd6497b0051a5
Ege Uysal <hi@egeuysal.com>
2026-08-16 09:43:40 -0500

    chore: update Zap formula

--- Formula/zap.rb
+++ Formula/zap.rb
@@ -9,21 +9,21 @@ class Zap < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/egeuysall/zap/releases/download/v0.1.3/zap-darwin-arm64"
-      sha256 "2e394db70b03f597092bd2bb10199c0843ccc14e1efacb02f9a802a4dcbb4506"
+      url "https://github.com/egeuysall/zap/releases/download/v0.1.4/zap-darwin-arm64"
+      sha256 "e51c619416ea8affeba950aae675a0fa92233bbcdb39ea1b89001ae88251b791"
     else
-      url "https://github.com/egeuysall/zap/releases/download/v0.1.3/zap-darwin-x64"
-      sha256 "3d2fb925d534c8e4d8a614ae8aa1ee55afdda5b509372a7f39d3286ed48d7aab"
+      url "https://github.com/egeuysall/zap/releases/download/v0.1.4/zap-darwin-x64"
+      sha256 "83b6e3d04b462702a076b1751d02f260a4931a4533a1099347827f9705dae946"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/egeuysall/zap/releases/download/v0.1.3/zap-linux-arm64"
-      sha256 "ff83bb7cc4076e2a79c05176385b57c86a126f2d6a7e91b5b02573547b139379"
+      url "https://github.com/egeuysall/zap/releases/download/v0.1.4/zap-linux-arm64"
+      sha256 "e69e1b58f2bf9642f72c6bbb4507b20aac0237c6ece43065bac047f1bcc8926e"
     else
-      url "https://github.com/egeuysall/zap/releases/download/v0.1.3/zap-linux-x64"
-      sha256 "15e22c217eca406c2a02baeb5efdb4b2c24af93fdb3ad727e262d52787eaa3f4"
+      url "https://github.com/egeuysall/zap/releases/download/v0.1.4/zap-linux-x64"
+      sha256 "590c9035a5835318fe8230baaf6c7cdf8bdcf238b6b9ef01b6ee4907af5ab080"
     end
   end
 
